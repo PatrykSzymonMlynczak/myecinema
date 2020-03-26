68183298ba47833cb0b4bb164d0be0a40f83b702
RadoslawGorniak <radekg08@wp.pl>
2020-03-26 16:16:45 +0100

    Initial commit

---
+++ src/test/java/com/wikingowie/myecinema/MyecinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wikingowie.myecinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyecinemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
